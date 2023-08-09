dc4c5d2bdf80bbfb95726d25d8a21df6b82e4962
sreedhar <sreedhar70755@gmail.com>
2023-08-09 08:19:45 +0530

    tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "servers" {
   for_each      = var.components
 
 
-  source            = "./git::https://github.com/sreedharm07/terraform-module.git"
+  source            = "git::https://github.com/sreedharm07/terraform-module.git"
   security_id       = var.id
   name              = each.value["name"]
   instance_type     = each.value["instance_type"]
